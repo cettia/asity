54212834cb711ea523c0013160b13567871d6928
Donghwan Kim <flowersinthesand@gmail.com>
2018-06-25 23:44:19 +0900

    Apply new layout

--- _includes/app.js
+++ _includes/app.js
@@ -1,23 +1,44 @@
-$(document).foundation();
-$("h2,h3").each(function() {
-  var $this = $(this);
-  var $link = $("<a />").attr("href", "#" + $this.attr('id')).text(String.fromCharCode("182"));
-  $this.append($link.hide()).hover(function() {
-    $link.show();
-  }, function () {
-    $link.hide();
+(function() {
+  $(document).foundation();
+
+  $("h2,h3").each(function() {
+    var $this = $(this);
+    var $link = $("<a />").attr("href", "#" + $this.attr('id')).text(String.fromCharCode("182"));
+    $this.append($link.hide()).hover(function() {
+      $link.show();
+    }, function () {
+      $link.hide();
+    });
   });
-});
-$('span.n:contains("httpAction"),span.n:contains("wsAction"),span.s:contains("\\"/asity\\"")').addClass('highlighted');
-$.get("http://cettia.io/feed.xml", function(doc) {
-  var $announcement = $("#announcement");
-  var $entry = $(doc).find("entry:contains(Asity):first");
 
-  var title = $entry.find("title").text();
-  $announcement.find(".link").attr({title: title, href: $entry.find("link").attr("href")}).text(title);
+  $('span.n:contains("httpAction"),span.n:contains("wsAction"),span.s:contains("\\"/path\\"")').addClass('highlighted');
 
-  var date = new Date(Date.parse($entry.find("published").text()));
-  var options = {year: "numeric", month: "long", day: "numeric"};
-  $announcement.find(".date").text(new Intl.DateTimeFormat("en-US", options).format(date));
-  $announcement.find("p").css({visibility: "visible"});
-});
+  var $tabs = $("#example-tabs");
+  var $tabsContent = $("div.tabs-content[data-tabs-content=" + $tabs.attr("id") + "]");
+  var $examples = $("div.example");
+  $tabs.find("li > a").each(function(i) {
+    var $this = $(this);
+    var $text = $this.text();
+    var $id = "example-" + $text.toLowerCase().replace(/\W/g, "-");
+    $this.attr("href", "#" + $id);
+    $("<div>").attr("id", $id).addClass("tabs-panel")
+      .append($("<p>").html("A working example is available at TODO example link."))
+      .append($($examples[i]).clone())
+      .append($("<p>").html("For more information, see <a href='#" + $id.substring(8) + "'>" + $text + "</a>."))
+      .appendTo($tabsContent);
+  });
+  new Foundation.Tabs($tabs);
+
+  $.get("http://cettia.io/feed.xml", function(doc) {
+    var $announcement = $("#announcement");
+    var $entry = $(doc).find("entry:contains(Asity):first");
+
+    var title = $entry.find("title").text();
+    $announcement.find(".link").attr({title: title, href: $entry.find("link").attr("href")}).text(title);
+
+    var date = new Date(Date.parse($entry.find("published").text()));
+    var options = {year: "numeric", month: "long", day: "numeric"};
+    $announcement.find(".date").text(new Intl.DateTimeFormat("en-US", options).format(date));
+    $announcement.find("p").css({visibility: "visible"});
+  });
+})();
